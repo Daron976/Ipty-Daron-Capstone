0267eac8fe66c2503805cbe8ab1869834cae72e7
Daron Wiafe <daronkwiafe@gmail.com>
2022-10-20 12:59:40 +0200

    Implement comment functionality

--- src/index.js
+++ src/index.js
@@ -26,11 +26,8 @@ const imgUrl = [
 ];
 
 const getList = async () => {
-  // const items = document.getElementById('list-items');
   const response = await fetch('https://api.openbrewerydb.org/breweries?by_type=nano&per_page=6');
   const data = await response.json();
-  // .then(response => response.json())
-  // .then(data => vm.feed = data);
 
   return data;
 };
@@ -38,7 +35,6 @@ const getList = async () => {
 const commentBtn = document.querySelectorAll('.comment-btn');
 const commentArray = Array.from(commentBtn);
 const popUpHolder = document.querySelector('.popup-holder');
-let dataArr;
 
 document.addEventListener('DOMContentLoaded', () => {
   getList()
@@ -49,15 +45,11 @@ document.addEventListener('DOMContentLoaded', () => {
         task.innerHTML += data[i].name;
       });
     });
-
   getList()
-    .then((data) => {
-      localStorage.setItem('API-result', JSON.stringify(data));
-      dataArr = JSON.parse(localStorage.getItem('API-result'));
-    });
+    .then((data) => displayPopUp(data));
 });
 
-async function postComment(url, dataObj) {
+const postComment = async (url, dataObj) => {
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify(dataObj),
@@ -66,34 +58,18 @@ async function postComment(url, dataObj) {
     },
   });
 
-  return response.json();
+  return response;
 }
 
-const getComment = async () => {
-  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WeiwNxFf2zcjWHsPu3w9/comments?item_id=item1')
+const getComment = async (id) => {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WeiwNxFf2zcjWHsPu3w9/comments?item_id=${id}`)
     .then((res) => res.json());
 
   return response;
 };
 
-const commentId = 0;
 
-document.querySelector('#comment-submission').addEventListener(() => {
-  const commentName = document.querySelector('#name');
-  const commentText = document.querySelector('.text');
-
-  const commentObj = {
-    item_id: commentId,
-    username: commentName.value,
-    comment: commentText.value,
-  }
-
-  commentId++;
-
-  postComment('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Gr2VdVSYrpHkHWrCrrcv/comments', commentObj);
-})
-
-const displayPopUp = async () => commentArray.forEach((btn) => {
+const displayPopUp = async (dataArr) => commentArray.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     for (let i = 0; i < dataArr.length; i += 1) {
       if ((dataArr[i].id === e.target.value) && (imgUrl[i].id === e.target.value)) {
@@ -122,6 +98,9 @@ const displayPopUp = async () => commentArray.forEach((btn) => {
               <div class="grid">
                 <div class="comments">
                   <h3>Comments</h3>
+                  <div class="comment-details">
+
+                  </div>
                 </div>
                 <div class="form-data">
                   <h3>Add a comment</h3>
@@ -131,7 +110,7 @@ const displayPopUp = async () => commentArray.forEach((btn) => {
                     <label for="text"></label>
                     <textarea name="text" id="text" cols="30" rows="5" placeholder="Your comment"></textarea>
                     <div class="btn-width">
-                      <button type="button" class="btn">Comment</button>
+                      <button type="button" class="btn" id="comment-submission">Comment</button>
                     </div>
                   </form>
                 </div>
@@ -146,31 +125,33 @@ const displayPopUp = async () => commentArray.forEach((btn) => {
           popUpHolder.removeChild(removed);
         });
 
-        // const commentId = 0;
+        document.querySelector('#comment-submission').addEventListener('click', () => {
+          const commentName = document.querySelector('#name');
+          const commentText = document.querySelector('#text');
 
-        // document.querySelector('#comment-submission').addEventListener(() => {
-        //   const commentName = document.querySelector('#name');
-        //   const commentText = document.querySelector('.text');
+          const commentObj = {
+            item_id: dataArr[i].id,
+            username: commentName.value,
+            comment: commentText.value,
+          }
 
-        //   const commentObj = {
-        //     item_id: commentId,
-        //     username: commentName.value,
-        //     comment: commentText.value,
-        //   }
+          postComment('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WeiwNxFf2zcjWHsPu3w9/comments', commentObj)
+            .then((response) => response.json())
+            .then((json) => console.log(json));
 
-        //   commentId++;
+          document.querySelector('.comment-details').innerHTML += `
+            <p>${commentName.value}: ${commentText.value}</p>
+          `;
+        })
 
-        //   postComment('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Gr2VdVSYrpHkHWrCrrcv/comments', commentObj);
-
-        //   getComment()
-        //     .then((data) => {
-        //       data.forEach((task) => {
-        //         document.querySelector('.comments').innerHTML += `
-        //           <p>${task.username}: ${task.comment}</p>
-        //         `;
-        //       })
-        //     });
-        // })
+        getComment(dataArr[i].id)
+        .then((data) => {
+          data.forEach((task) => {
+            document.querySelector('.comment-details').innerHTML += `
+              <p>${task.username}: ${task.comment}</p>
+            `;
+          })
+        });
       }
     }
   });
