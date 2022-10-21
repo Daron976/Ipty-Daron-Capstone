95db30417347f16e45251bda43a59f75f4d46076
Daron Wiafe <daronkwiafe@gmail.com>
2022-10-21 09:04:18 +0200

    Implement comment functionality with responsive styling

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ const imgUrl = [
   },
   {
     id: 'brockopp-brewing-valley-city',
-    url: 'https://i.pinimg.com/564x/95/c0/8e/95c08eb331e50847b6be8e2de19e3afe.jpg',
-  },
-  {
-    id: 'brouwerij-les-deplorables-woodinville',
-    url: 'https://i.pinimg.com/564x/07/2a/dd/072add42318c1aaefd083bc258503568.jpg',
-  },
-  {
-    id: 'gnarly-cedar-brewing-greenleaf',
     url: 'https://i.pinimg.com/564x/93/c3/76/93c37604ff7280e5c7977399def0d1ca.jpg',
   },
   {
+    id: 'brouwerij-les-deplorables-woodinville',
+    url: 'https://i.pinimg.com/564x/95/c0/8e/95c08eb331e50847b6be8e2de19e3afe.jpg',
+  },
+  {
+    id: 'gnarly-cedar-brewing-greenleaf',
+    url: 'https://images.unsplash.com/photo-1582106245687-cbb466a9f07f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+  },
+  {
     id: 'oxford-brewing-company-grand-forks',
-    url: 'https://i.pinimg.com/564x/3b/cf/d3/3bcfd3b3d8e6b9d4d6af787ee50b3364.jpg',
+    url: 'https://i.pinimg.com/564x/07/2a/dd/072add42318c1aaefd083bc258503568.jpg',
   },
 ];
 
@@ -55,46 +55,50 @@ const commentBtn = document.querySelectorAll('.comment-btn');
 const commentArray = Array.from(commentBtn);
 const popUpHolder = document.querySelector('.popup-holder');
 
-const displayPopUp = async (dataArr) => commentArray.forEach((btn) => {
+const displayPopUp = async (dataArr) => commentArray.forEach((btn, i) => {
   btn.addEventListener('click', (e) => {
+    e.target.value = dataArr[i].id;
     for (let i = 0; i < dataArr.length; i += 1) {
       if ((dataArr[i].id === e.target.value) && (imgUrl[i].id === e.target.value)) {
         const popupContent = `
-          <div class="popup-window">
+          <div class="popup-window flex">
             <div class="top-layer">
-              <div class="data-img">
+              <div class="data-img flex">
                 <img 
                 src="${imgUrl[i].url}"; 
                 alt="brewery with alcohol tanks">
-                <i class="fa-solid fa-x fa-2x"></i>
+                <span class="material-symbols-outlined close-popup">cancel</span>
               </div>
               <div>
                 <h2 class="popup-window-header">
                   ${dataArr[i].name}
                 </h2>
                 <div class="popup-info">
-                  <p class="type">Brewery type: ${dataArr[i].brewery_type}</p>
-                  <p class="country">Country: ${dataArr[i].country}</p>
-                  <p class="state">State: ${dataArr[i].state}</p>
-                  <p class="city">City: ${dataArr[i].city}</p>
-                  <p class="website">Webpage: <a href="${dataArr[i].webpage_url}">${dataArr[i].name}</a></p>
-                  <p class="number">${dataArr[i].phone}</p>
-                </div>
-              </div>
-              <div class="grid">
-                <div class="comments">
-                  <h3>Comments</h3>
-                  <div class="comment-details">
-
+                  <div class="popup-info-section">
+                    <p class="info-item"><strong>Brewery type</strong>: ${dataArr[i].brewery_type}</p>
+                    <p class="info-item"><strong>Country</strong>: ${dataArr[i].country}</p>
+                    <p class="info-item"><strong>State</strong>: ${dataArr[i].state}</p>
+                  </div>
+                  <div class="popup-info-section">
+                    <p class="info-item"><strong>City</strong>: ${dataArr[i].city}</p>
+                    <p class="info-item"><strong>Webpage</strong>: <a href="${dataArr[i].webpage_url}">${dataArr[i].name}</a></p>
+                    <p class="info-item"><strong>Phone</strong>: ${dataArr[i].phone}</p>
                   </div>
                 </div>
-                <div class="form-data">
+              </div>
+              <div class="user-input">
+                <div class="comments">
+                  <h3>Comments</h3>
+                  <div class="comment-details flex"></div>
+                </div>
+                <div class="form-data flex">
                   <h3>Add a comment</h3>
-                  <form action="#">
+                  <form action="#" class="flex">
                     <label for="name"></label>
                     <input type="text" id="name" name="name" placeholder="Your name">
                     <label for="text"></label>
                     <textarea name="text" id="text" cols="30" rows="5" placeholder="Your comment"></textarea>
+                    <small id="empty-fields"><span class="material-symbols-outlined">error</span> All fields are required</small>
                     <div class="btn-width">
                       <button type="button" class="btn" id="comment-submission">Comment</button>
                     </div>
@@ -106,7 +110,7 @@ const displayPopUp = async (dataArr) => commentArray.forEach((btn) => {
 
         popUpHolder.insertAdjacentHTML('afterbegin', popupContent);
 
-        document.querySelector('.fa-x').addEventListener('click', () => {
+        document.querySelector('.close-popup').addEventListener('click', () => {
           const removed = document.querySelector('.popup-window');
           popUpHolder.removeChild(removed);
         });
@@ -115,19 +119,24 @@ const displayPopUp = async (dataArr) => commentArray.forEach((btn) => {
           const commentName = document.querySelector('#name');
           const commentText = document.querySelector('#text');
 
-          const commentObj = {
-            item_id: dataArr[i].id,
-            username: commentName.value,
-            comment: commentText.value,
-          };
+          if ((commentName.value === '') || (commentText.value === '')) {
+            document.getElementById('empty-fields').style.display = 'flex';
+          } else {
+            document.getElementById('empty-fields').style.display = 'none';
+            const commentObj = {
+              item_id: dataArr[i].id,
+              username: commentName.value,
+              comment: commentText.value,
+            };
 
-          postComment('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WeiwNxFf2zcjWHsPu3w9/comments', commentObj)
-            .then((response) => response.json())
-            .then((json) => json);
+            postComment('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WeiwNxFf2zcjWHsPu3w9/comments', commentObj)
+              .then((response) => response.json())
+              .then((json) => json);
 
-          document.querySelector('.comment-details').innerHTML += `
-            <p>${commentName.value}: ${commentText.value}</p>
-          `;
+            document.querySelector('.comment-details').innerHTML += `
+              <p>${commentName.value}: ${commentText.value}</p>
+            `;
+          }
         });
 
         getComment(dataArr[i].id)
@@ -147,9 +156,14 @@ document.addEventListener('DOMContentLoaded', () => {
   getList()
     .then((data) => {
       const nameHolder = document.querySelectorAll('.brewery-name');
+      const breweryImg = document.querySelectorAll('.bre-img');
       const arr = Array.from(nameHolder);
       arr.forEach((task, i) => {
-        task.innerHTML += data[i].name;
+        if ((task.id === data[i].id) && (imgUrl[i].id === data[i].id)) {
+          task.innerHTML += data[i].name;
+          breweryImg[i].setAttribute('src', imgUrl[i].url);
+          breweryImg[i].setAttribute('alt', data[i].name);
+        }
       });
     });
   getList()
@@ -157,3 +171,14 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 displayPopUp();
+
+document.getElementById('menu-open').addEventListener('click', () => {
+  // console.log('test')
+  document.getElementById('mobile-menu').style.display = 'flex';
+  document.getElementById('menu-open').style.display = 'none';
+});
+
+document.querySelector('#menu-close').addEventListener('click', () => {
+  document.getElementById('mobile-menu').style.display = 'none';
+  document.getElementById('menu-open').style.display = 'flex';
+});
