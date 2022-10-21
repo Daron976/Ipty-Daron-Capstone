8d69f1ce08267ea9a21dc8e73baddda4b6fc433c
Iptysam <mukadamiptisam@gmail.com>
2022-10-21 23:51:25 +0300

    Update the itemcount function

--- src/modules/itemsCount.js
+++ src/modules/itemsCount.js
@@ -1,5 +1,5 @@
 const itemCounterFunction = () => {
-  const breweryinfo = document.querySelector('.homepage-content flex');
+  const breweryinfo = document.querySelector('.homepage-content');
   let counter = 0;
   counter = breweryinfo.getElementsByTagName('img').length;
   return counter;
